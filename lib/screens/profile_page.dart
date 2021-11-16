f474c350de43841adb5e3522d901de5b6b5a7321
FlutLab.io <>
2021-11-16 12:50:14 +0000

    Autosave

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -3,7 +3,7 @@ import 'package:ecommerce_int2/screens/faq_page.dart';
 import 'package:ecommerce_int2/screens/payment/payment_page.dart';
 import 'package:ecommerce_int2/screens/settings/settings_page.dart';
 import 'package:ecommerce_int2/screens/tracking_page.dart';
-import 'package:ecommerce_int2/screens/wallet/wallet_page.dart';
+import 'package:ecommerce_int2/screens/wallet/temp.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
@@ -15,8 +15,7 @@ class ProfilePage extends StatelessWidget {
         top: true,
         child: SingleChildScrollView(
           child: Padding(
-            padding:
-                EdgeInsets.only(left: 16.0, right: 16.0, top: kToolbarHeight),
+            padding: EdgeInsets.only(left: 16.0, right: 16.0, top: kToolbarHeight),
             child: Column(
               children: <Widget>[
                 CircleAvatar(
@@ -32,20 +31,9 @@ class ProfilePage extends StatelessWidget {
                 ),
                 Container(
                   margin: EdgeInsets.symmetric(vertical: 16.0),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(8),
-                          topRight: Radius.circular(8),
-                          bottomLeft: Radius.circular(8),
-                          bottomRight: Radius.circular(8)),
-                      color: Colors.white,
-                      boxShadow: [
-                        BoxShadow(
-                            color: transparentYellow,
-                            blurRadius: 4,
-                            spreadRadius: 1,
-                            offset: Offset(0, 1))
-                      ]),
+                  decoration: BoxDecoration(borderRadius: BorderRadius.only(topLeft: Radius.circular(8), topRight: Radius.circular(8), bottomLeft: Radius.circular(8), bottomRight: Radius.circular(8)), color: Colors.white, boxShadow: [
+                    BoxShadow(color: transparentYellow, blurRadius: 4, spreadRadius: 1, offset: Offset(0, 1))
+                  ]),
                   height: 150,
                   child: Center(
                     child: Row(
@@ -56,9 +44,7 @@ class ProfilePage extends StatelessWidget {
                           children: <Widget>[
                             IconButton(
                               icon: Image.asset('assets/icons/wallet.png'),
-                              onPressed:()=> Navigator.of(context).push(
-                                  MaterialPageRoute(
-                                      builder: (_) => WalletPage())),
+                              onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: (_) => WalletPage())),
                             ),
                             Text(
                               'Wallet',
@@ -71,8 +57,7 @@ class ProfilePage extends StatelessWidget {
                           children: <Widget>[
                             IconButton(
                               icon: Image.asset('assets/icons/truck.png'),
-                              onPressed: () => Navigator.of(context).push(
-                                MaterialPageRoute(builder: (_) => TrackingPage())),
+                              onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: (_) => TrackingPage())),
                             ),
                             Text(
                               'Shipped',
@@ -85,9 +70,7 @@ class ProfilePage extends StatelessWidget {
                           children: <Widget>[
                             IconButton(
                               icon: Image.asset('assets/icons/card.png'),
-                              onPressed:()=> Navigator.of(context).push(
-                                  MaterialPageRoute(
-                                      builder: (_) => PaymentPage())),
+                              onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: (_) => PaymentPage())),
                             ),
                             Text(
                               'Payment',
@@ -99,7 +82,8 @@ class ProfilePage extends StatelessWidget {
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: <Widget>[
                             IconButton(
-                              icon: Image.asset('assets/icons/contact_us.png'), onPressed: () {},
+                              icon: Image.asset('assets/icons/contact_us.png'),
+                              onPressed: () {},
                             ),
                             Text(
                               'Support',
@@ -114,10 +98,14 @@ class ProfilePage extends StatelessWidget {
                 ListTile(
                   title: Text('Settings'),
                   subtitle: Text('Privacy and logout'),
-                  leading: Image.asset('assets/icons/settings_icon.png', fit: BoxFit.scaleDown, width: 30, height: 30,),
+                  leading: Image.asset(
+                    'assets/icons/settings_icon.png',
+                    fit: BoxFit.scaleDown,
+                    width: 30,
+                    height: 30,
+                  ),
                   trailing: Icon(Icons.chevron_right, color: yellow),
-                  onTap: () => Navigator.of(context).push(
-                      MaterialPageRoute(builder: (_) => SettingsPage())),
+                  onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: (_) => SettingsPage())),
                 ),
                 Divider(),
                 ListTile(
@@ -135,8 +123,7 @@ class ProfilePage extends StatelessWidget {
                   subtitle: Text('Questions and Answer'),
                   leading: Image.asset('assets/icons/faq.png'),
                   trailing: Icon(Icons.chevron_right, color: yellow),
-                  onTap: () => Navigator.of(context).push(
-                      MaterialPageRoute(builder: (_) => FaqPage())),
+                  onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: (_) => FaqPage())),
                 ),
                 Divider(),
               ],
