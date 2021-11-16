d4abbbe9fb06ca424ab01be64f974b8e67eb8f39
FlutLab.io <>
2021-11-16 15:08:13 +0000

    Autosave

--- lib/screens/auth/welcome_back_page.dart
+++ lib/screens/auth/welcome_back_page.dart
@@ -13,7 +13,7 @@ class WelcomeBackPage extends StatefulWidget {
 
 class _WelcomeBackPageState extends State<WelcomeBackPage> {
   final _scaffoldKey = GlobalKey<ScaffoldState>();
-  final _formKey = GlobalKey<FormState>();
+  final _formKey = new GlobalKey();
 
   bool _isSubmitting = false, _obscureText = true;
   String? _email = "", _password = "";
