2a3c682ca3a973474d7adf56da9167872144ec4a
FlutLab.io <>
2021-11-16 15:25:56 +0000

    Autosave

--- lib/screens/auth/sidebar.dart
+++ lib/screens/auth/sidebar.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:ecommerce_int2/screens/auth/login_page.dart';
+import 'package:ecommerce_int2/screens/auth/welcome_back_page.dart';
 
 class sideBar extends StatelessWidget {
   @override
@@ -24,15 +24,15 @@ class sideBar extends StatelessWidget {
             ),
             decoration: BoxDecoration(color: Colors.green),
           ),
-          ListTile(leading: Icon(Icons.home), title: Text('Home'), onTap: () => Navigator.push(context, new MaterialPageRoute(builder: (context) => new LoginPage()))),
-          ListTile(leading: Icon(Icons.person), title: Text('Profile'), onTap: () => Navigator.push(context, new MaterialPageRoute(builder: (context) => new LoginPage()))),
-          ListTile(leading: Icon(Icons.subscriptions), title: Text('Subscriptions'), onTap: () => Navigator.push(context, new MaterialPageRoute(builder: (context) => new LoginPage()))),
-          ListTile(leading: Icon(Icons.add_shopping_cart), title: Text('Your Orders'), onTap: () => Navigator.push(context, new MaterialPageRoute(builder: (context) => new LoginPage()))),
+          ListTile(leading: Icon(Icons.home), title: Text('Home'), onTap: () => Navigator.push(context, new MaterialPageRoute(builder: (context) => new WelcomeBackPage()))),
+          ListTile(leading: Icon(Icons.person), title: Text('Profile'), onTap: () => Navigator.push(context, new MaterialPageRoute(builder: (context) => new WelcomeBackPage()))),
+          ListTile(leading: Icon(Icons.subscriptions), title: Text('Subscriptions'), onTap: () => Navigator.push(context, new MaterialPageRoute(builder: (context) => new WelcomeBackPage()))),
+          ListTile(leading: Icon(Icons.add_shopping_cart), title: Text('Your Orders'), onTap: () => Navigator.push(context, new MaterialPageRoute(builder: (context) => new WelcomeBackPage()))),
           Divider(),
-          ListTile(leading: Icon(Icons.web), title: Text('Delivery Address(es)'), onTap: () => Navigator.push(context, new MaterialPageRoute(builder: (context) => new LoginPage()))),
-          ListTile(leading: Icon(Icons.question_answer_outlined), title: Text('FAQS'), onTap: () => Navigator.push(context, new MaterialPageRoute(builder: (context) => new LoginPage()))),
+          ListTile(leading: Icon(Icons.web), title: Text('Delivery Address(es)'), onTap: () => Navigator.push(context, new MaterialPageRoute(builder: (context) => new WelcomeBackPage()))),
+          ListTile(leading: Icon(Icons.question_answer_outlined), title: Text('FAQS'), onTap: () => Navigator.push(context, new MaterialPageRoute(builder: (context) => new WelcomeBackPage()))),
           Divider(),
-          ListTile(title: Text('Sign In'), leading: Icon(Icons.person_add), onTap: () => Navigator.push(context, new MaterialPageRoute(builder: (context) => new LoginPage()))),
+          ListTile(title: Text('Sign In'), leading: Icon(Icons.person_add), onTap: () => Navigator.push(context, new MaterialPageRoute(builder: (context) => new WelcomeBackPage()))),
         ],
       ),
     );
