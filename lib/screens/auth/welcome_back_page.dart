a11061308ef8ff7d8d996bce550bf27ca19bfee3
FlutLab.io <>
2021-11-17 05:56:44 +0000

    Autosave

--- lib/screens/auth/welcome_back_page.dart
+++ lib/screens/auth/welcome_back_page.dart
@@ -25,13 +25,16 @@ class _WelcomeBackPageState extends State<WelcomeBackPage> {
   }
 
   Widget _showEmailInput() {
-    return Padding(padding: EdgeInsets.only(top: 20.0), child: Container(margin: const EdgeInsets.only(right: 10, left: 10), child: TextFormField(onSaved: (val) => _email = val, validator: (val) => val?.length != 10 ? 'Invalid Mobile Number' : null, decoration: InputDecoration(border: OutlineInputBorder(), labelText: 'Mobile Number *', hintText: 'Mobile Number *'))));
+    return Padding(padding: EdgeInsets.only(top: 20.0), child:
+    Container(
+  margin: const EdgeInsets.only(right: 10, left: 10),
+  child: 
+    TextFormField(onSaved: (val) => _email = val, validator: (val) => val?.length != 10 ? 'Invalid Mobile Number' : null, decoration: InputDecoration(border: OutlineInputBorder(), labelText: 'Mobile Number *', hintText: 'Mobile Number *')));
   }
 
   Widget _showPasswordInput() {
     return Padding(
         padding: EdgeInsets.only(top: 20.0),
-        Container(margin: const EdgeInsets.only(right: 10, left: 10),
         child: TextFormField(
             onSaved: (val) => _password = val,
             validator: (val) => val == null
@@ -49,7 +52,7 @@ class _WelcomeBackPageState extends State<WelcomeBackPage> {
               border: OutlineInputBorder(),
               labelText: 'Password *',
               hintText: 'Password *',
-            ))));
+            )));
   }
 
   Widget _showFormActions() {
