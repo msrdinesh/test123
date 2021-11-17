a80717afad352ca0a7e6e471098707faf2b58504
FlutLab.io <>
2021-11-17 05:07:19 +0000

    Autosave

--- lib/screens/auth/welcome_back_page.dart
+++ lib/screens/auth/welcome_back_page.dart
@@ -201,6 +201,27 @@ class _WelcomeBackPageState extends State<WelcomeBackPage> {
     );
 
     return Scaffold(
+      appBar: AppBar(
+          title: RichText(
+            textAlign: TextAlign.center,
+            text: TextSpan(children: <TextSpan>[
+              TextSpan(text: "Feed", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 40)),
+              TextSpan(text: "Next", style: TextStyle(color: Colors.yellow, fontWeight: FontWeight.bold, fontSize: 40)),
+            ]),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.green,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.add_shopping_cart,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                // do something
+              },
+            )
+          ]),
       body: Stack(
         children: <Widget>[
           Container(
