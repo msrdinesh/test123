1adbc6e5edab8765375548713372d2412cbd85fd
FlutLab.io <>
2021-11-18 07:44:07 +0000

    Autosave

--- lib/screens/auth/welcome_back_page.dart
+++ lib/screens/auth/welcome_back_page.dart
@@ -140,6 +140,8 @@ class _WelcomeBackPageState extends State<WelcomeBackPage> {
 
         final FirebaseAuth _auth = FirebaseAuth.instance;
         print(_auth);
+        print(_email.toString());
+        print(_password.toString());
         FirebaseUser user = await _auth.signInWithEmailAndPassword(email: _email.toString(), password: _password.toString());
         print("here i am there");
         if (user == null) {
