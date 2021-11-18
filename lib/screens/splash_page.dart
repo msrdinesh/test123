b4af6d77f780a3d69f948abf64f656ccd4d108ce
FlutLab.io <>
2021-11-18 11:59:30 +0000

    Autosave

--- lib/screens/splash_page.dart
+++ lib/screens/splash_page.dart
@@ -1,5 +1,6 @@
 import 'package:ecommerce_int2/app_properties.dart';
 import 'package:ecommerce_int2/screens/auth/welcome_back_page.dart';
+import 'package:ecommerce_int2/screens/auth/register_page.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -7,16 +8,14 @@ class SplashScreen extends StatefulWidget {
   _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen>
-    with SingleTickerProviderStateMixin {
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
   late Animation<double> opacity;
   late AnimationController controller;
 
   @override
   void initState() {
     super.initState();
-    controller = AnimationController(
-        duration: Duration(milliseconds: 2500), vsync: this);
+    controller = AnimationController(duration: Duration(milliseconds: 2500), vsync: this);
     opacity = Tween<double>(begin: 1.0, end: 0.0).animate(controller)
       ..addListener(() {
         setState(() {});
@@ -33,15 +32,12 @@ class _SplashScreenState extends State<SplashScreen>
   }
 
   void navigationPage() {
-    Navigator.of(context)
-        .pushReplacement(MaterialPageRoute(builder: (_) => WelcomeBackPage()));
+    Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) => RegisterPage()));
   }
 
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage('assets/background.jpg'), fit: BoxFit.cover)),
+      decoration: BoxDecoration(image: DecorationImage(image: AssetImage('assets/background.jpg'), fit: BoxFit.cover)),
       child: Container(
         decoration: BoxDecoration(color: transparentYellow),
         child: SafeArea(
@@ -49,21 +45,15 @@ class _SplashScreenState extends State<SplashScreen>
             body: Column(
               children: <Widget>[
                 Expanded(
-                  child: Opacity(
-                      opacity: opacity.value,
-                      child: new Image.asset('assets/logo.png')),
+                  child: Opacity(opacity: opacity.value, child: new Image.asset('assets/logo.png')),
                 ),
                 Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: RichText(
-                    text: TextSpan(
-                        style: TextStyle(color: Colors.black),
-                        children: [
-                          TextSpan(text: 'Powered by '),
-                          TextSpan(
-                              text: 'int2.io',
-                              style: TextStyle(fontWeight: FontWeight.bold))
-                        ]),
+                    text: TextSpan(style: TextStyle(color: Colors.black), children: [
+                      TextSpan(text: 'Powered by '),
+                      TextSpan(text: 'int2.io', style: TextStyle(fontWeight: FontWeight.bold))
+                    ]),
                   ),
                 )
               ],
