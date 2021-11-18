1d25a5072f857c86471f4555b2ce1d306e3f2bcc
FlutLab.io <>
2021-11-18 15:02:31 +0000

    Autosave

--- lib/screens/main/homePage.dart
+++ lib/screens/main/homePage.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:ecommerce_int2/screens/add_contact.dart';
+import 'package:ecommerce_int2/screens/view_screen.dart';
+import 'package:ecommerce_int2/screens/edit_screen.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,8 +11,28 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  DatabaseReference _databaseReference = FirebaseDatabase.instance.reference();
+
+  navigatToAddScreen() {
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return AddContact();
+    }));
+  }
+
+  navigatToViewScreen(id) {
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      //TODO id;
+      return ViewContact();
+    }));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: navigatToAddScreen,
+      ),
+    );
   }
 }
