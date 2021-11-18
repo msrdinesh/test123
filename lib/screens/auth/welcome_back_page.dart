e4c5c47f63fe0df08bb90cae820129e2a2b75946
FlutLab.io <>
2021-11-18 11:51:04 +0000

    Autosave

--- lib/screens/auth/welcome_back_page.dart
+++ lib/screens/auth/welcome_back_page.dart
@@ -50,7 +50,7 @@ class _WelcomeBackPageState extends State<WelcomeBackPage> {
   @override
   void initState() {
     super.initState();
-    // this.checkAuthentication();
+    this.checkAuthentication();
   }
 
   bool _isSubmitting = false, _obscureText = true;
