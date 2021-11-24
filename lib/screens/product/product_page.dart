dcaa498102c63a3129d4d745e0959daebf5f6a62
FlutLab.io <>
2021-11-24 10:15:57 +0000

    Autosave

--- lib/screens/product/product_page.dart
+++ lib/screens/product/product_page.dart
@@ -45,12 +45,7 @@ class _ProductPageState extends State<ProductPage> {
             ],
             borderRadius: BorderRadius.circular(9.0)),
         child: Center(
-          child: Text("View Product",
-              style: const TextStyle(
-                  color: const Color(0xfffefefe),
-                  fontWeight: FontWeight.w600,
-                  fontStyle: FontStyle.normal,
-                  fontSize: 20.0)),
+          child: Text("View Product", style: const TextStyle(color: const Color(0xfffefefe), fontWeight: FontWeight.w600, fontStyle: FontStyle.normal, fontSize: 20.0)),
         ),
       ),
     );
@@ -67,14 +62,12 @@ class _ProductPageState extends State<ProductPage> {
               'assets/icons/search_icon.svg',
               fit: BoxFit.scaleDown,
             ),
-            onPressed: () => Navigator.of(context)
-                .push(MaterialPageRoute(builder: (_) => SearchPage())),
+            onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: (_) => SearchPage())),
           )
         ],
         title: Text(
           'Headphones',
-          style: const TextStyle(
-              color: darkGrey, fontWeight: FontWeight.w500, fontSize: 18.0),
+          style: const TextStyle(color: darkGrey, fontWeight: FontWeight.w500, fontSize: 18.0),
         ),
       ),
       body: Stack(
@@ -96,10 +89,7 @@ class _ProductPageState extends State<ProductPage> {
                   padding: const EdgeInsets.only(left: 20.0, right: 16.0),
                   child: Text(
                     product.name,
-                    style: const TextStyle(
-                        color: const Color(0xFFFEFEFE),
-                        fontWeight: FontWeight.w600,
-                        fontSize: 20.0),
+                    style: const TextStyle(color: const Color(0xFFFEFEFE), fontWeight: FontWeight.w600, fontSize: 20.0),
                   ),
                 ),
                 SizedBox(
@@ -115,16 +105,10 @@ class _ProductPageState extends State<ProductPage> {
                         decoration: BoxDecoration(
                           color: Color.fromRGBO(253, 192, 84, 1),
                           borderRadius: BorderRadius.circular(4.0),
-                          border:
-                              Border.all(color: Color(0xFFFFFFFF), width: 0.5),
+                          border: Border.all(color: Color(0xFFFFFFFF), width: 0.5),
                         ),
                         child: Center(
-                          child: new Text("Details",
-                              style: const TextStyle(
-                                  color: const Color(0xeefefefe),
-                                  fontWeight: FontWeight.w300,
-                                  fontStyle: FontStyle.normal,
-                                  fontSize: 12.0)),
+                          child: new Text("Details", style: const TextStyle(color: const Color(0xeefefefe), fontWeight: FontWeight.w300, fontStyle: FontStyle.normal, fontSize: 12.0)),
                         ),
                       )
                     ],
@@ -133,33 +117,20 @@ class _ProductPageState extends State<ProductPage> {
                 SizedBox(
                   height: 16.0,
                 ),
-                Padding(
-                    padding:
-                        EdgeInsets.only(left: 20.0, right: 40.0, bottom: 130),
-                    child: new Text(product.description,
-                        style: const TextStyle(
-                            color: const Color(0xfefefefe),
-                            fontWeight: FontWeight.w800,
-                            fontFamily: "NunitoSans",
-                            fontStyle: FontStyle.normal,
-                            fontSize: 16.0)))
+                Padding(padding: EdgeInsets.only(left: 20.0, right: 40.0, bottom: 130), child: new Text(product.description, style: const TextStyle(color: const Color(0xfefefefe), fontWeight: FontWeight.w800, fontFamily: "NunitoSans", fontStyle: FontStyle.normal, fontSize: 16.0)))
               ],
             ),
           ),
           Align(
             alignment: Alignment.bottomCenter,
             child: Container(
-              padding: EdgeInsets.only(
-                  top: 8.0, bottom: bottomPadding != 20 ? 20 : bottomPadding),
+              padding: EdgeInsets.only(top: 8.0, bottom: bottomPadding != 20 ? 20 : bottomPadding),
               decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      colors: [
-                    Color.fromRGBO(255, 255, 255, 0),
-                    Color.fromRGBO(253, 192, 84, 0.5),
-                    Color.fromRGBO(253, 192, 84, 1),
-                  ],
-                      begin: FractionalOffset.topCenter,
-                      end: FractionalOffset.bottomCenter)),
+                  gradient: LinearGradient(colors: [
+                Color.fromRGBO(255, 255, 255, 0),
+                Color.fromRGBO(253, 192, 84, 0.5),
+                Color.fromRGBO(253, 192, 84, 1),
+              ], begin: FractionalOffset.topCenter, end: FractionalOffset.bottomCenter)),
               width: width,
               height: 120,
               child: Center(child: viewProductButton),
