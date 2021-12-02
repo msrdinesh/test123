08230cf7d12f06516c97d26bf8ebe24100c8f4b8
FlutLab.io <>
2021-12-02 22:38:38 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -7,19 +7,17 @@ import 'package:e_commers/Routes/Routes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
- 
+
 void main() => runApp(MyApp());
- 
-class MyApp extends StatelessWidget 
-{
+
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) 
-  {
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarColor: Colors.transparent, statusBarIconBrightness: Brightness.dark ));
-      
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarColor: Colors.transparent, statusBarIconBrightness: Brightness.dark));
+
     return MultiBlocProvider(
       providers: [
-        BlocProvider(create: (context) => AuthBloc()..add( CheckLoginEvent() ) ),
+        BlocProvider(create: (context) => AuthBloc()..add(CheckLoginEvent())),
         BlocProvider(create: (context) => GeneralBloc()),
         BlocProvider(create: (context) => ProductBloc()),
         BlocProvider(create: (context) => PersonalBloc()),
