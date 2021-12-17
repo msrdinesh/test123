d88d52979eb1daca4a3ee4563638ae4a4b95b1d5
FlutLab.io <>
2021-12-17 06:45:36 +0000

    Autosave

--- lib/screens/feedbackform/feedbackform.dart
+++ lib/screens/feedbackform/feedbackform.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-// import 'dart:ffi';
+import 'dart:ffi';
 import 'dart:typed_data';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cornext_mobile/components/widgets/appbarwidget.dart';
