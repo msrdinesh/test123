c1618c1f6059b0f6cbceb0aefb8f9ec349a313ee
FlutLab.io <>
2021-12-19 16:01:21 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -109,7 +109,7 @@ class MyAppState extends State<MyApp> {
 
   void func() async {
     var prefManager = await SharedPreferences.getInstance();
-    await prefManager.clear();
+    // await prefManager.clear();
     language = prefManager.getString("SelectedLanguageCode");
   }
 
