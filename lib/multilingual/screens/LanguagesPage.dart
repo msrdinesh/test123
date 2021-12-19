aa10cb15cfa8b8db02d8555e41201f02e06c03d5
FlutLab.io <>
2021-12-19 09:04:13 +0000

    Autosave

--- lib/multilingual/screens/LanguagesPage.dart
+++ lib/multilingual/screens/LanguagesPage.dart
@@ -112,7 +112,7 @@ class LanguagePageState extends State<LanguagePage> {
                   onPressed: () {},
                   child: Text('TextButton'))
               : SizedBox(height: 0, width: 0),
-          SiedBox(height: 100)
+          SizedBox(height: 100)
         ])));
   }
 }
