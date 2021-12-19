644a419c4978663181a2e77d3ed78c36414df3aa
FlutLab.io <>
2021-12-19 09:14:25 +0000

    Autosave

--- lib/components/widgets/appbarwidget.dart
+++ lib/components/widgets/appbarwidget.dart
@@ -409,7 +409,7 @@ Widget appBarDrawer(context, state, GlobalKey<ScaffoldState> scaffoldKey) {
         // ),
 
         ListTile(
-          title: Text(Languages.of(context).appName),
+          title: Text(Languages.of(context).home),
           leading: Icon(Icons.home),
           onTap: () {
             Navigator.pushNamedAndRemoveUntil(context, '/home', ModalRoute.withName('/home'));
