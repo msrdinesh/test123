ee4829c8feb9632d14941114de2a826091ee5a1c
FlutLab.io <>
2021-12-19 13:53:53 +0000

    Autosave

--- lib/multilingual/localization/language/language_be.dart
+++ lib/multilingual/localization/language/language_be.dart
@@ -3,4 +3,10 @@ import 'languages.dart';
 class LanguageBe extends Languages {
   @override
   String get home => "বাড়ি";
+  String get profile => "Profile";
+  String get subscriptions => "Subscriptions";
+  String get yourOrders => "Your Orders";
+  String get deliveryAddress => "Delivery Address(es)";
+  String get faqs => "FAQS";
+  String get logout => "Logout";
 }
