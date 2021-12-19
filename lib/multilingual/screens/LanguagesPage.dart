3c0e8d0edf20dce6c478b2d6740e5a3f5a073cd2
FlutLab.io <>
2021-12-19 08:06:18 +0000

    Autosave

--- lib/multilingual/screens/LanguagesPage.dart
+++ lib/multilingual/screens/LanguagesPage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:cornext_mobile/multilingual/localization/language/languages.dart';
+import 'package:cornext_mobile/multilingual/localization/locale_constant.dart';
+import 'package:cornext_mobile/multilingual/model/language_data.dart';
 
 class LanguagesPage extends StatelessWidget {
   @override
