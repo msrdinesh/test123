65d236659bf07f290835465de5c10fce5e8bbfe6
FlutLab.io <>
2021-12-19 09:09:05 +0000

    Autosave

--- lib/multilingual/screens/LanguagesPage.dart
+++ lib/multilingual/screens/LanguagesPage.dart
@@ -103,7 +103,7 @@ class LanguagePageState extends State<LanguagePage> {
                           child: SelectCard(choice: choices[7]),
                         ))
                   ]))),
-          _isSelected ? FlatButton(onPressed: () {}, color: Colors.yellow, minWidth: 300.0, child: Text('Contineu in English')) : SizedBox(height: 0, width: 0),
+          _isSelected ? FlatButton(onPressed: () {}, color: Colors.yellow, minWidth: 350.0, child: Text('Contineu in English')) : SizedBox(height: 0, width: 0),
           SizedBox(height: 230)
         ])));
   }
