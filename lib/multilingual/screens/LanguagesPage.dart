5eb5a345c40998ccd40cfe52c42bca0f16d5e8ab
FlutLab.io <>
2021-12-19 08:07:30 +0000

    Autosave

--- lib/multilingual/screens/LanguagesPage.dart
+++ lib/multilingual/screens/LanguagesPage.dart
@@ -17,7 +17,9 @@ class LanguagesPage extends StatelessWidget {
                   child: GridView.count(childAspectRatio: (2 / 1), crossAxisCount: 2, crossAxisSpacing: 4.0, mainAxisSpacing: 8.0, children: [
                     InkWell(
                         onTap: () => {
-                              changeLanguage(context, "en")
+                              changeLanguage(context, "en");
+                              print("pressed");
+
                             },
                         child: Center(
                           child: SelectCard(choice: choices[0]),
