af6ff657809b983f69452f20c97e3e6a84e48b6d
FlutLab.io <>
2021-12-20 15:16:59 +0000

    Autosave

--- lib/screens/home/homescreen.dart
+++ lib/screens/home/homescreen.dart
@@ -1354,13 +1354,14 @@ class HomeScreen extends State<HomePage> {
           height: 100,
           width: 100,
           child: Card(
+              borderRadius: BorderRadius.circular(10),
               child: Column(children: [
-            Align(alignment: Alignment.centerLeft, child: Text(addressList[i]['doorNumber'])),
-            Align(alignment: Alignment.centerLeft, child: Text(addressList[i]['street'])),
-            Align(alignment: Alignment.centerLeft, child: Text(addressList[i]['city'])),
-            Align(alignment: Alignment.centerLeft, child: Text(addressList[i]['state'])),
-            Align(alignment: Alignment.centerLeft, child: Text(addressList[i]['mobileNo']))
-          ]))));
+                Align(alignment: Alignment.centerLeft, child: Text(addressList[i]['doorNumber'])),
+                Align(alignment: Alignment.centerLeft, child: Text(addressList[i]['street'])),
+                Align(alignment: Alignment.centerLeft, child: Text(addressList[i]['city'])),
+                Align(alignment: Alignment.centerLeft, child: Text(addressList[i]['state'])),
+                Align(alignment: Alignment.centerLeft, child: Text(addressList[i]['mobileNo']))
+              ]))));
     }
     return ans;
   }
