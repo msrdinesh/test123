56c5c090d4f9b1fa721bc8a7af4de87342c97c39
FlutLab.io <>
2021-12-20 11:38:41 +0000

    Autosave

--- lib/screens/home/homescreen.dart
+++ lib/screens/home/homescreen.dart
@@ -900,8 +900,8 @@ class HomeScreen extends State<HomePage> {
                                 child: CachedNetworkImage(
                                   imageUrl: 'https://feednextmedia.s3.ap-south-1.amazonaws.com/CAT-Icon-All-01.jpg',
                                   fit: BoxFit.fill,
-                                  height: 30,
-                                  width: 30,
+                                  height: 5,
+                                  width: 5,
                                 ))),
                         AppStyles().customPadding(1),
                         Container(
