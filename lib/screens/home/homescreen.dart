6663bf1a4ceba90b377a7cda866ac10fc9c1724a
FlutLab.io <>
2021-12-21 12:33:09 +0000

    Autosave

--- lib/screens/home/homescreen.dart
+++ lib/screens/home/homescreen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:cornext_mobile/components/widgets/appbarwidget.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:geolocator/geolocator.dart';
 import 'dart:convert';
 import 'package:cornext_mobile/constants/appcolors.dart';
 import 'package:cornext_mobile/services/refreshtokenservice/refreshtokenservice.dart';
@@ -1543,6 +1544,43 @@ class HomeScreen extends State<HomePage> {
         });
   }
 
+  void getCoordinatesAndPlace() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error('Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    var pos = await Geolocator.getCurrentPosition();
+    print(pos);
+  }
+
   void _showCurrentLocation(VoidCallback fun) {
     showModalBottomSheet(
         context: context,
@@ -1576,7 +1614,7 @@ class HomeScreen extends State<HomePage> {
                     RaisedButton(
                       color: Colors.yellow,
                       onPressed: () {
-                        print('Pressed');
+                        Navigator.pop(context);
                       },
                       child: SizedBox(width: MediaQuery.of(context).size.width * 0.38, height: 50, child: Center(child: Text("Not now"))),
                     ),
@@ -1584,7 +1622,8 @@ class HomeScreen extends State<HomePage> {
                     RaisedButton(
                       color: mainAppColor,
                       onPressed: () {
-                        print('Pressed');
+                        getCoordinatesAndPlace();
+                        fun();
                       },
                       child: SizedBox(width: MediaQuery.of(context).size.width * 0.38, height: 50, child: Center(child: Text("Allow access"))),
                     )
