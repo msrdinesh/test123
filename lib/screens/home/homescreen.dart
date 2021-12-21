b5c3e6320349a67d8734af98c512635118c7f1d2
FlutLab.io <>
2021-12-21 09:25:54 +0000

    Autosave

--- lib/screens/home/homescreen.dart
+++ lib/screens/home/homescreen.dart
@@ -1549,7 +1549,7 @@ class HomeScreen extends State<HomePage> {
           return FractionallySizedBox(
               heightFactor: 0.6,
               child: Column(children: [
-                Text("Allow location access to improve shopping experience")
+                Padding(padding: EdgeInsets.all(15), child: Text("Allow location access to improve shopping experience", style: TextStyle(fontSize: 20)))
               ]));
         });
   }
