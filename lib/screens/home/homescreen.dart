e8eb4d0f40f28d4e82909b2805df89dce5ca310f
FlutLab.io <>
2021-12-21 07:20:50 +0000

    Autosave

--- lib/screens/home/homescreen.dart
+++ lib/screens/home/homescreen.dart
@@ -1433,6 +1433,7 @@ class HomeScreen extends State<HomePage> {
     print(form);
     if (form.validate()) {
       form.save();
+      setState() {}
       print(pincode);
       Navigator.pop(context);
     } else {
